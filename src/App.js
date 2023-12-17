bd694078b1f6ac58a37cc9c6045ebcb2150b1a75
Nabhag8848 <nabhagmotivaras76@gmail.com>
2023-12-17 16:55:31 +0530

    handling useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+export default function App() {
+  const [currencies, setCurrencies] = useState({});
+  const [from, setFrom] = useState("USD");
+  const [curNumber, setCurNumber] = useState(1);
+  const [to, setTo] = useState("INR");
+  const [converted, setConverted] = useState("");
+
+  useEffect(function () {
+    async function getCurrencies() {
+      const res = await fetch("https://api.frankfurter.app/currencies");
+      const data = await res.json();
+      setCurrencies(data);
+    }
+
+    getCurrencies();
+
+    return function () {
+      setCurrencies({});
+    };
+  }, []);
+
+  useEffect(
+    function () {
+      async function getConversion() {
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${curNumber}&from=${from}&to=${to}`
+        );
+
+        const data = await res.json();
+        setConverted(data.rates[to]);
+      }
+      if (curNumber !== "" && curNumber !== 0) {
+        getConversion();
+      }
+    },
+    [from, curNumber, to]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Input setCurNumber={setCurNumber} curNumber={curNumber} />
+      <DropDown
+        curSelected={from}
+        setCurSelected={setFrom}
+        currencies={currencies}
+        alreadySelect={to}
+      />
+      <DropDown
+        curSelected={to}
+        setCurSelected={setTo}
+        currencies={currencies}
+        alreadySelect={from}
+      />
+
+      <h2>
+        {converted} {to}
+      </h2>
     </div>
   );
 }
 
-export default App;
+function DropDown({ curSelected, setCurSelected, currencies, alreadySelect }) {
+  const filterCurrency = { ...currencies };
+  delete filterCurrency?.[alreadySelect];
+
+  return (
+    <select
+      value={curSelected}
+      onChange={(e) => {
+        setCurSelected(e.target.value);
+      }}
+    >
+      {Object.entries(filterCurrency).map(([currency, fullname]) => {
+        return (
+          <option value={currency} key={currency}>
+            {fullname}
+          </option>
+        );
+      })}
+    </select>
+  );
+}
+
+function Input({ setCurNumber, curNumber }) {
+  return (
+    <input
+      type="number"
+      onChange={(e) => {
+        if (e.target.value.trim() === "") {
+          setCurNumber("");
+          return;
+        }
+        const amount = Number(e.target.value);
+        setCurNumber(amount);
+      }}
+      value={curNumber}
+    ></input>
+  );
+}
